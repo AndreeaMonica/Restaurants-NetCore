c4886509ae694047009721db455c40bf675176f5
Andreea Monica Stănilă <stanila.andreea96@gmail.com>
2019-10-27 20:32:53 +0200

    Add Reposirories ,Models and Mockup data

---
+++ RestaurantsWeb/Repositories/IRestaurantRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantsWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantsWeb.Repositories
+{
+    public interface IRestaurantRepository
+    {
+        RestaurantDTO GetRestaurant(string name);
+    }
+}
---
+++ RestaurantsWeb/Repositories/RestaurantRepository.cs
@@ -0,0 +1,33 @@
+﻿using RestaurantsWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantsWeb.Repositories
+{
+    public class RestaurantRepository : IRestaurantRepository
+    {
+
+        private readonly List<RestaurantDTO> context;
+
+        public RestaurantRepository()
+        {
+            context = new List<RestaurantDTO>(); // Response
+            context.Add(new RestaurantDTO { Name = "Mara Mura", Location = "Aviatorilor" });
+            context.Add(new RestaurantDTO { Name = "Ethel", Location = "Arcul de Triumf" });
+            context.Add(new RestaurantDTO { Name = "French Revolution", Location = "Victoriei" });
+            context.Add(new RestaurantDTO { Name = "Maison des Crepes", Location = "Aviatorilor" });
+        }
+
+        public RestaurantDTO GetRestaurant(string name)
+        {
+            var restaurant = context.FirstOrDefault(x => x.Name == name);
+            if (restaurant == null)
+            {
+                throw new Exception("Invalid name");
+            }
+            return restaurant;
+        }
+    }
+}
